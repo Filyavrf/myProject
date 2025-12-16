f9aa163e470f8f4297473c1ccb7bab9695616166
Vladf <vlad.filenko98@mail.ru>
2025-12-16 21:45:09 +0700

    осталось сделать 3 мелких сайта

---
+++ script.js
@@ -0,0 +1,34 @@
+let phrases = [
+
+  { text: 'отправить другу смешную фотку', image: '/images/page-5/cat.png' },
+
+  { text: 'посидеть в Telegram', image: '/images/page-5/cat1.png' },
+
+  { text: 'посчитать мышей', image: '/images/page-5/cat2.png' },
+
+  { text: 'посмеяться с котиков', image: '/images/page-5/cat3.png' },
+
+  { text: 'посмотреть видео в YouTube', image: '/images/page-5/cat4.png' },
+
+  { text: 'полистать Reels', image: '/images/page-5/cat5.png' },
+
+  { text: 'почитать новости', image: '/images/page-5/cat6.png' },
+
+];
+
+function getRandomElement(arr) {
+  let randIndex = Math.floor(Math.random() * arr.length);
+  return arr[randIndex];
+}
+
+let button = document.querySelector('.content5__button');
+
+let phrase = document.querySelector('.content5__description-phrase');
+
+let image = document.querySelector('.content5__image');
+
+button.addEventListener('click', function () {
+  let randomElement = getRandomElement(phrases);
+  smoothly(phrase, 'textContent', randomElement.text);
+  smoothly(image, 'src', randomElement.image);
+});
